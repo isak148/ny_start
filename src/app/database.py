c3c6ab444644a2202a4bae87bf7579c327eee121
Isak Rosendahl Heggland <isak.heggland36@gmail.com>
2026-03-27 10:48:31 +0100

    Frontend

--- src/app/database.py
+++ src/app/database.py
@@ -2,12 +2,14 @@ from sqlmodel import Field, SQLModel,create_engine,Session
 
 #HEr definerervi tabellen for brukere
 
+# Sjekk at dette stemmer i src/app/database.py
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(unique=True,index=True)
-    email: str = Field(unique=True,index=True)
-    password: str
-    default_station: str = Field(default="SN18700") #Denne skal være mulig å endre for alle brukere på sikt
+    username: str = Field(unique=True, index=True)
+    email: str # <-- Sannsynligvis lagt til
+    password: str # <-- Sannsynligvis lagt til
+    default_station: str = Field(default="SN18700")
+    favorites: str = Field(default="")
 
     # lagrer favoritt-statuser for brukeren, som komma-separert string
     # Eksempel: "SN18700,SN50540,SN90450"
@@ -19,6 +21,7 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 #Oppretter selve database-motoren
 engine = create_engine(sqlite_url, connect_args={"check_same_thread":False})
+
 
 #funksjon som oppretter tabellene i databasen om de ikke allerede finnes
 def create_db_and_tables():
