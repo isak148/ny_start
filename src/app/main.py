4cfa1a1cd016644490bb68724f97938ae78c5262
Isak Rosendahl Heggland <isak.heggland36@gmail.com>
2026-03-27 08:50:08 +0100

    user setup

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from sqlmodel import Session, select
+from contextlib import asynccontextmanager
 import uvicorn
 
 from app.frost_api import hent_vaerdata
 from frcm.fireriskmodel.compute import compute
-
-from contextlib import asynccontextmanager
-from app.database import create_db_and_tables
+from app.database import create_db_and_tables, get_session, User
 
 #Dette sørger for at databasen opprettes i det sekundet serveren starter.
 @asynccontextmanager
@@ -45,8 +45,33 @@ def get_risk(station_id: str):
     except Exception as e:
         return{"error": f"Det oppsto en feil: {str(e)}"}
 
+@app.post("/users/")
+def create_user(user: User, session: Session = Depends(get_session())):
+    """opretter en ny bruker i databasen"""
+    #1. Sjekk om brukernavn allerede finnes
+    statement = select(User).where(User.username == user.username)
+    ekisterende_bruker = session.exec(statement).first()
 
-    #Kjør kode ved nedstenging av serveren (om nødvendig)
+    if ekisterende_bruker:
+        raise HTTPException(status_code=400, detail="Brukernavnet er allerede i bruk")
+    #2. Lagre den nye brukeren i databsen
+    session.add(user)
+    session.commit()
+    session.refresh(user) #oppdaterer variabelenslik at vi får ID-en databasen ga den
+
+    return user
+
+@app.get("/user/{username}")
+def get_user(username: str, session: Session = Depends(get_session)):
+    """Henter informasjonom en spesifikk bruker"""
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="fant ingen bruker med dette brukernavnet")
+    
+    return user 
+
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
